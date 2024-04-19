aeb79aaaf12617d305b8087f50da6b8308a8ce47
Ryan Montoyo <ryanmontoyo2003@gmail.com>
2024-04-19 16:27:32 -0400

    react

---
+++ twitter-clone/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import SideBar from "./SideBar";
+import './App.css';
+
+
+function App() {
+  return (
+    // BEM Convention
+    <div className="app">
+      <h1>Hey Programmers... Let's build a twitter clone</h1>
+      
+      {/* SIDEBAR */}
+
+      {/* FEED */}
+
+      {/* WIDGETS */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ twitter-clone/src/SideBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Sidebar.css';
+import TwitterIcon from "@material-ui/icons/Twitter";
+
+function SideBar() {
+  return (
+    <div className='sidebar'>
+        {/*twitter icon*/}
+        <TwitterIcon />
+        {/* sidear option*/}
+        {/* sidear option*/}
+        {/* sidear option*/}
+        {/* sidear option*/}
+        {/* sidear option*/}
+        {/* sidear option*/}
+        {/* sidear option*/}
+
+        {/* button -> tweet */}
+      
+    </div>
+  )
+}
+
+export default SideBar
